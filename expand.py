0e6c08c24922a4a4207b8dc3ccf8059b20a2034b
esay286 <esay286@aucklanduni.ac.nz>
2016-11-04 15:36:50 +1300

    Added basic program structure with dummy functions

--- expand.py
+++ expand.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3
+
+def Expand(fname):
+    print("TODO: Expand " + fname)
+
+def Unexpand(fname):
+    print("TODO: Unexpand " + fname)
+
+def req_code(msg, valid):
+    """Repeatedly ask for input until one of the valid answers is recieved
+    msg: A message to repeatedly display the user to prompt for an answer
+    valid: a list of valid responses"""
+    response = ""
+    while not response in valid:
+        response = input(msg).lower()
+    return response
+
+functs  = dict({"e": Expand, "u": Unexpand})
+
+if __name__ == "__main__":
+    while True:
+        mode = req_code("Expand (e) or Unexpand (u) a file: ", functs.keys())
+        fname = input("Enter filename: ")
+        functs[mode](fname)
