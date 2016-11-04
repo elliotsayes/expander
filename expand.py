2ac325c7a399b4cfe38d8c6d7657f7a48eaf5693
esay286 <esay286@aucklanduni.ac.nz>
2016-11-04 16:22:21 +1300

    Added ability to use command line arguments.

--- expand.py
+++ expand.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
+import sys
 
-def Expand(fname):
+def expand(fname):
     print("TODO: Expand " + fname)
 
-def Unexpand(fname):
+def unexpand(fname):
     print("TODO: Unexpand " + fname)
 
 def req_code(msg, valid):
@@ -15,10 +16,27 @@ def req_code(msg, valid):
         response = input(msg).lower()
     return response
 
-functs  = dict({"e": Expand, "u": Unexpand})
+def print_help():
+    print("""Usage: expand.py [-h] [-e <filename> | -u <filename>]
+-h : show this help
+-e : expand file
+-u : unexpand file""")
 
-if __name__ == "__main__":
+def interactive():
     while True:
         mode = req_code("Expand (e) or Unexpand (u) a file: ", functs.keys())
         fname = input("Enter filename: ")
         functs[mode](fname)
+
+functs  = dict({"e": expand, "u": unexpand})
+
+if __name__ == "__main__":
+    if len(sys.argv) is 1:
+        interactive()
+    else:
+        try:
+            command = sys.argv[1][1:]
+            fname = sys.argv[2]
+            functs[command](fname)
+        except (IndexError, KeyError):
+            print_help()
