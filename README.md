f4c602d2b72da35d2a71672e26924006bb415944
elliotsayes <elliotsayes@gmail.com>
2016-11-05 21:04:27 +1300

    Added ability to specify expansion code in both command line and interactive mode. A bit of restructuring was required. Fixed some readme formatting too

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 "Expansion" software written in python 3. Like compression software, but the other way around.
 
 ### Usage
-    expand.py [-h] [-e <filename> N | -u <filename>]
+    Usage: expand.py [-h] [-e <filename> <N> | -u <filename>]
     -h : show this help
-    -e : expand file
+    -e : expand file (N = expansion code)
     -u : unexpand file
 Run with no arguments for interactive mode
 
@@ -13,11 +13,11 @@ Takes in input file name from the [/files/examples](/files/examples) folder, and
 The process is reversable. You can take a big file from the [/files/expanded](/files/expanded) folder and unexpand it. The output is placed in the [/files/unexpanded](/files/unexpanded) folder.
 
 ### How?
-Each byte in the file is expanded to be `R` (the expansion ratio) times bigger. `R` must be calculable from the equation `R = 2^N`, where `N` (the expansion code) is an integer between 0 and 255. Therefore `R` can be as low as one and up to `57 quattuorvigintillion 896 trevigintillion 44 duovigintillion 618 unvigintillion 658 vigintillion 97 novemdecillion 711 octodecillion 785 septendecillion 492 sexdecillion 504 quindecillion 343 quattuordecillion 953 tredecillion 926 duodecillion 634 undecillion 992 decillion 332 nonillion 820 octillion 282 septillion 19 sextillion 728 quintillion 792 quadrillion 3 trillion 956 billion 564 million 819 thousand 969`. So probably dont use `N = 255`.
+Each byte in the file is expanded to be `R` (the expansion ratio) times bigger. `R` must be calculable from the equation `R = 2^N`, where `N` (the expansion code) is an integer between 0 and 255. Therefore `R` can be as low as 1 and up to 57 quattuorvigintillion 896 trevigintillion 44 duovigintillion 618 unvigintillion 658 vigintillion 97 novemdecillion 711 octodecillion 785 septendecillion 492 sexdecillion 504 quindecillion 343 quattuordecillion 953 tredecillion 926 duodecillion 634 undecillion 992 decillion 332 nonillion 820 octillion 282 septillion 19 sextillion 728 quintillion 792 quadrillion 3 trillion 956 billion 564 million 819 thousand 969. So probably dont use `N = 255`.
 
 The expansion code `N` is written to the first byte of the  expanded file. The rest of the file consists of each of the bytes in the input file expanded by ratio `R`. The expansion process for a byte `B` is simply to fill the first `R-1` bytes with random data, and set the last byte to `(B-S)%256` (where `S` is the sum of all the previous bytes). This makes the equation `B = S%256` work.
 
 The unexpansion process starts by reading the first byte in the expanded file to find `N`, then calculate `R` through the equation `R = 2^N`. The expanded file is then read in in groups of `R` bytes, and the original byte is calculated from the sum of these bytes using the formula `B = S%256` (where S is the sum, B is the original byte).
 
 ### Why?
-Useful if you want to waste someone else's (and your own) bandwidth to aquire a file. Other than that I don't know, for fun?
+Useful if you want to waste someone else's (and your own) bandwidth to transfer a file, dial-up simulator. Other than that I don't know, for fun?
--- expand.py
+++ expand.py
@@ -2,8 +2,6 @@
 import sys
 import os
 
-write_expansion_code = 1
-
 source_base_dir = "files/examples/"
 expanded_base_dir = "files/expanded/"
 unexpanded_base_dir = "files/unexpanded/"
@@ -31,33 +29,65 @@ def unexpand_bytes(byte_sequence):
         byte_sum += byte
     return bytes([byte_sum % 256])
 
-def expand_file(fname):
+def expand_file(fname,n):
     fname_in = os.path.join(source_base_dir,fname)
     fname_out = os.path.join(expanded_base_dir,fname)
 
-    with open(fname_in, 'rb') as fo_in, open(fname_out, 'wb') as fo_out:
-        fo_out.write(bytes([write_expansion_code]))
-        ratio = 2**write_expansion_code
+    try:
+        with open(fname_in, 'rb') as fo_in, open(fname_out, 'wb') as fo_out:
+            fo_out.write(bytes([n]))
+            ratio = 2**n
+            print("Expanding with ratio "+str(ratio))
 
-        byte = fo_in.read(1)
-        while byte:
-            expanded_byte = expand_byte(byte, ratio)
-            fo_out.write(expanded_byte)
             byte = fo_in.read(1)
+            while byte:
+                expanded_byte = expand_byte(byte, ratio)
+                fo_out.write(expanded_byte)
+                byte = fo_in.read(1)
+    except FileNotFoundError:
+        print("Error: Could not find file: " + fname_in)
 
 def unexpand_file(fname):
     fname_in = os.path.join(expanded_base_dir,fname)
     fname_out = os.path.join(unexpanded_base_dir,fname)
 
-    with open(fname_in, 'rb') as fo_in, open(fname_out, 'wb') as fo_out:
-        ex_code = fo_in.read(1)
-        ratio = 2**ex_code[0]
+    try:
+        with open(fname_in, 'rb') as fo_in, open(fname_out, 'wb') as fo_out:
+            ex_code = fo_in.read(1)
+            ratio = 2**ex_code[0]
+            print("Unexpanding with ratio "+str(ratio))
 
-        byte_sequence = fo_in.read(ratio)
-        while byte_sequence:
-            unexpanded_byte = unexpand_bytes(byte_sequence)
-            fo_out.write(unexpanded_byte)
             byte_sequence = fo_in.read(ratio)
+            while byte_sequence:
+                unexpanded_byte = unexpand_bytes(byte_sequence)
+                fo_out.write(unexpanded_byte)
+                byte_sequence = fo_in.read(ratio)
+    except FileNotFoundError:
+        print("Error: Could not find file: " + fname_in)
+
+def expand_file_int():
+    fname = input("File to expand: ")
+    n = int(input("Expansion code: "))
+    expand_file(fname,n)
+
+def unexpand_file_int():
+    fname = input("File to unexpand: ")
+    unexpand_file(fname)
+
+def expand_file_cmd(argv):
+    try:
+        fname = argv[2]
+        n = int(argv[3])
+        expand_file(fname,n)
+    except (IndexError,ValueError):
+        print_help()
+
+def unexpand_file_cmd(argv):
+    try:
+        fname = argv[2]
+        unexpand_file(fname)
+    except IndexError:
+        print_help()
 
 def req_code(msg, valid):
     """Repeatedly ask for input until one of the valid answers is recieved
@@ -69,20 +99,20 @@ def req_code(msg, valid):
     return response
 
 def print_help():
-    print("""Usage: expand.py [-h] [-e <filename> | -u <filename>]
+    print("""Usage: expand.py [-h] [-e <filename> <N> | -u <filename>]
 -h : show this help
--e : expand file
+-e : expand file (N = expansion code)
 -u : unexpand file
 
 Run with no arguments for interactive mode""")
 
+int_functs = dict({"e": expand_file_int, "u": unexpand_file_int, "q": exit})
+cmd_functs = dict({"-e": expand_file_cmd, "-u": unexpand_file_cmd})
+
 def interactive():
     while True:
-        mode = req_code("Expand (e) or Unexpand (u) a file: ", functs.keys())
-        fname = input("Enter filename: ")
-        functs[mode](fname)
-
-functs  = dict({"e": expand_file, "u": unexpand_file})
+        command = req_code("Expand (e) or Unexpand (u) a file (q to quit): ", int_functs.keys())
+        int_functs[command]()
 
 def test_byte(n,r):
     e = expand_byte(bytes([n]), r)
@@ -93,13 +123,12 @@ def test_byte(n,r):
 
 if __name__ == "__main__":
     path_check()
-    #test_byte(0,8)
+    #test_byte(0,2)
     if len(sys.argv) is 1:
         interactive()
     else:
         try:
-            command = sys.argv[1][1:]
-            fname = sys.argv[2]
-            functs[command](fname)
+            command = sys.argv[1]
+            cmd_functs[command](sys.argv)
         except (IndexError, KeyError):
             print_help()
